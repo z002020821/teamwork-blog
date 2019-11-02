cdf3aaf605dcfb0a379b27de5c9e49b572341f66
rolf0504 <fangrolf@gmail.com>
2019-11-02 23:50:34 +0800

    html structure

--- public/js/login.js
+++ public/js/login.js
@@ -11,9 +11,9 @@ $('#login').click(function () {
 })
 
 socket.on('page1', function () {
-    window.location.replace("../html/stats/userPage.html")
+    window.location.replace("../html/login/userPage.html")
 })
 
 socket.on('page2', function () {
-    window.location.replace("../html/stats/error.html")
+    window.location.replace("../html/login/error.html")
 })
--- public/js/register.js
+++ public/js/register.js
@@ -11,9 +11,9 @@ $('#regist').click(function () {
 })
 
 socket.on('created', function () {
-    window.location.replace("../html/stats/success.html")
+    window.location.replace("../html/register/success.html")
 })
 
 socket.on('failed', function () {
-    window.location.replace("../html/stats/fail.html")
+    window.location.replace("../html/register/fail.html")
 })
