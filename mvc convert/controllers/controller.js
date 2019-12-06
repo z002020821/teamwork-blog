1d348c224263523e14dd73bdff2d113e74699e59
koozu <cl4cl4cl40726fff@gmail.com>
2019-12-06 20:32:10 +0800

    modify controller.js

--- mvc convert/controllers/controller.js
+++ mvc convert/controllers/controller.js
@@ -12,20 +12,20 @@ async function Controller(srv) {
 
         socket.on('getId', function (data) {
             if (data.sessionId == null) {
-                var session_id = uuidv4()
-                socket.room = session_id
-                socket.join(socket.room, function (res) {
-                  socket.emit('idConfirm', {
-                    sessionId: session_id
-                  })
+              var session_id = uuidv4()
+              socket.room = session_id
+              socket.join(socket.room, function (res) {
+                socket.emit('idConfirm', {
+                  sessionId: session_id
                 })
+              })
             } else {
-                socket.room = data.sessionId
-                socket.join(socket.room, function (res) {
-                  socket.emit('idConfirm', {
-                    sessionId: data.sessionId
-                  })
+              socket.room = data.sessionId
+              socket.join(socket.room, function (res) {
+                socket.emit('idConfirm', {
+                  sessionId: data.sessionId
                 })
+              })
             }
         })
 
@@ -33,66 +33,78 @@ async function Controller(srv) {
           var uuid = data.sessionId
           var recAcnt = data.acnt
           var recPwd = data.pwd
-
-          var modelCursor = model.getCursor('password', 'users')
-          modelCursor.then(function (cursor) {
-            cursor.each(function (err, doc) {
-              if (doc) {
-                var userAcnt = doc.account
-                var userPwd = doc.password
-
-                if (recAcnt == userAcnt) {
-                  if (recPwd == userPwd) {
-                    var insertData = JSON.stringify({
-                      account: userAcnt, 
-                      uuid: uuid
-                    })
-                    model.userLogin('loginUsers', 'users', insertData)
-                    socket.emit('switchPage', {
-                      'page':'userPage'
-                    })
-                  } else {
-                    socket.emit('switchPage', {
-                      'page':'err'
-                    })
-                  }
-                }
-              } else {
-                socket.emit('switchPage', {
-                  'page':'err'
-                })
-              }
+          
+          if (recAcnt == "" || recPwd == "") {
+            socket.emit('switchPage', {
+              'page':'err'
             })
-          })
+          } else {
+            var modelCursor = model.getCursor('password', 'users')
+            modelCursor.then(function (cursor) {
+              cursor.each(function (err, doc) {
+                if (doc) {
+                  var userAcnt = doc.account
+                  var userPwd = doc.password
+
+                  if (recAcnt == userAcnt) {
+                    if (recPwd == userPwd) {
+                      var insertData = JSON.stringify({
+                        account: userAcnt, 
+                        uuid: uuid
+                      })
+                      model.userLogin('loginUsers', 'users', insertData)
+                      socket.emit('switchPage', {
+                        'page':'userPage'
+                      })
+                    } else {
+                      socket.emit('switchPage', {
+                        'page':'err'
+                      })
+                    }
+                  }
+                } else {
+                  socket.emit('switchPage', {
+                    'page':'err'
+                  })
+                }
+              })
+            })
+          }
         })
         
         socket.on('regist', function (data) {
           var recAcnt = data.acnt
           var recPwd = data.pwd
-
-          var modelCursor = model.getCursor('password', 'users')
-          modelCursor.then(function (cursor) {
-            cursor.each(function (err, doc) {
-              if (doc) {
-                var userAcnt = doc.account
-
-                if (recAcnt == userAcnt) {
-                  socket.emit('switchRegistPage', {
-                    'page':'fail'
-                  })
-                } else {
-                  var insertData = JSON.stringify({
-                    account: recAcnt, 
-                    password: recPwd
-                  })
-                  model.regist('password', 'users', insertData)
-                  socket.emit('switchRegistPage', {
-                    'page':'sucess'
-                  })
-                }
-              }
+          
+          if (recAcnt == "" || recPwd == "") {
+            socket.emit('switchRegistPage', {
+              'page':'fail'
             })
-          })
+          } else {
+            var modelCursor = model.getCursor('password', 'users')
+            modelCursor.then(function (cursor) {
+              cursor.each(function (err, doc) {
+                if (doc) {
+                  var userAcnt = doc.account
+
+                  if (recAcnt == userAcnt) {
+                    socket.emit('switchRegistPage', {
+                      'page':'fail'
+                    })
+                  } else {
+                    var insertData = JSON.stringify({
+                      account: recAcnt, 
+                      password: recPwd
+                    })
+                    model.regist('password', 'users', insertData)
+                    socket.emit('switchRegistPage', {
+                      'page':'sucess'
+                    })
+                  }
+                }
+              })
+            })
+          }
         })
 
         socket.on('po', function (data) {
@@ -100,30 +112,36 @@ async function Controller(srv) {
           var recContent = data.content
           var recPublisher = data.publisher
 
-          var modelCursor = model.getCursor('post', 'userpost')
-          modelCursor.then(function (cursor) {
-            cursor.each(function (err, doc) {
-              if (doc) {
-                var title = doc.title
-
-                if (recTitle == title) {
-                  socket.emit('switchPostPage', {
-                    'page':'fail'
-                  })
-                } else {
-                  var insertData = JSON.stringify({
-                    title: recTitle, 
-                    content: recContent,
-                    publisher: recPublisher
-                  })
-                  model.po('post', 'userpost', insertData)
-                  socket.emit('switchPostPage', {
-                    'page':'sucess'
-                  })
-                }
-              }
+          if (recTitle = "") {
+            socket.emit('switchPostPage', {
+              'page':'fail'
             })
-          })
+          } else {
+            var modelCursor = model.getCursor('post', 'userpost')
+            modelCursor.then(function (cursor) {
+              cursor.each(function (err, doc) {
+                if (doc) {
+                  var title = doc.title
+  
+                  if (recTitle == title) {
+                    socket.emit('switchPostPage', {
+                      'page':'fail'
+                    })
+                  } else {
+                    var insertData = JSON.stringify({
+                      title: recTitle, 
+                      content: recContent,
+                      publisher: recPublisher
+                    })
+                    model.po('post', 'userpost', insertData)
+                    socket.emit('switchPostPage', {
+                      'page':'sucess'
+                    })
+                  }
+                }
+              })
+            })
+          } 
         })
 
         socket.on('disconnect', function () {
