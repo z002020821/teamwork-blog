2e921f09a3c5c9239049c6ab608369dc86d5aa3e
z002020821 <z0020020821@gmail.com>
2019-12-16 19:08:14 +0800

    view -- create post

--- mvc convert/views/view.js
+++ mvc convert/views/view.js
@@ -2,21 +2,33 @@ var pug = require('pug')
 var socket = io()
 
 //register's pageSwitch
-socket.on('created', function () {
-    window.location.replace("../public/html/register/success.html")
+socket.on('switchRegistPage', function () {
+    if('page' === 'sucess') {
+        window.location.replace("../public/html/register/success.html")
+    } else if ('page' === 'fail') {
+        window.location.replace("../public/html/register/fail.html")
+    }
 })
-
-socket.on('failed', function () {
-    window.location.replace("../public/html/register/fail.html")
-})
-
 
 //login's pageSwitch
-socket.on('switchPage', function(){
+socket.on('switchLoginPage', function(){
     if ('page'==='userPage') {
         window.location.replace("../public/html/userpage/home.html")
     } else if ('page'==='err'){
         window.location.replace("../public/html/login/error.html")
+    }
+})
+
+socket.on('switchPage',function() {
+    window.location.replace("../public/html/login/error.html")
+})
+
+//post's pageSwitch
+socket.on('switchPostPage', function() {
+    if('page' === 'sucess') {
+        window.location.replace("../public/html/userpage/home.html")
+    } else if ('page' === 'fail') {
+        window.location.replace("../public/html/register/fail.html")
     }
 })
 
